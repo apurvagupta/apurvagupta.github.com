e5c4b58709fb8cb2cf24a48a7256930db4d9e1b5
apurvagu <apurvagupta0@gmail.com>
2015-02-28 21:48:33 +0530

    started animations

--- js/landing-page.js
+++ js/landing-page.js
@@ -6,3 +6,36 @@
 }
 });
 
+  $(document).ready(function(){
+	$('.scroll-link').on('click',function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			'scrollTop': $($(this).attr('href')).position().top-55
+		},700);
+	});
+
+	
+	new WOW().init();
+	$("html").niceScroll({
+		zindex:999999,
+		boxzoom:true,
+		cursoropacitymin :0.5,
+		cursoropacitymax :0.8,
+		cursorwidth :"10px",
+		cursorborder :"0px solid",
+		autohidemode:false
+	});
+
+	$(window).scroll(function(){
+		// Keep navbar background default color intially, make it transparent when you're scrolling
+		var windowScroll = $(window).scrollTop(),
+			windowHeight = $(window).height();
+		if (windowScroll > 1) {
+			$('.hoverr-navbar').addClass('scrolled');
+		}else if (windowScroll < 1){
+			$('.hoverr-navbar').removeClass('scrolled');
+		};
+	})
+
+});
+
